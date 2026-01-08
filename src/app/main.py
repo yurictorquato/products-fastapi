1e196b7ac845416c15abbf254f43b818fcac059e
Yuri <yurictorquato@gmail.com>
2026-01-07 22:53:34 -0300

    refactor: adicionando as rotas no app

--- src/app/main.py
+++ src/app/main.py
@@ -5,6 +5,7 @@ from app.handlers import (
     invalid_product_data_exception_handler,
     product_not_found_exception_handler,
 )
+from app.routers import api_router
 
 app = FastAPI(
     title="Products FastAPI", description="API para gerenciamento de produtos"
@@ -14,3 +15,5 @@ app.add_exception_handler(ProductNotFoundException, product_not_found_exception_
 app.add_exception_handler(
     InvalidProductDataException, invalid_product_data_exception_handler
 )
+
+app.include_router(api_router)
